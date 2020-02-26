6a0f62948ccaa3ef3079a9cd2f8833f60858dde8
Ostap Yakovenko <ceo@crnd.pro>
2020-02-26 13:20:33 +0200

    Fixes

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Yodoo Apps',
-    'version': '12.0.0.12.1',
+    'version': '12.0.0.12.2',
 
     # any module necessary for this one to work correctly
     'depends': [
