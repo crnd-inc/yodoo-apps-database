8358a84d874d645c8e16ce843c471ef5ea2575d2
Dmytro Katyukha <cto@crnd.pro>
2020-12-01 14:42:19 +0200

    Improve UI for module view (show deps as list instead of tags)

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Yodoo Apps',
-    'version': '12.0.0.16.0',
+    'version': '12.0.0.17.0',
 
     # any module necessary for this one to work correctly
     'depends': [
