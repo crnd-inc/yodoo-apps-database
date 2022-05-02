7a1483506e6f81f0ce5523e54ddea8c051b8468f
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-05-02 21:26:29 +0300

    [14.0] Port to 14.0

--- yodoo_apps_database/__manifest__.py
+++ yodoo_apps_database/__manifest__.py
@@ -7,7 +7,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Yodoo Apps',
-    'version': '13.0.0.41.0',
+    'version': '14.0.0.41.0',
 
     # any module necessary for this one to work correctly
     'depends': [
