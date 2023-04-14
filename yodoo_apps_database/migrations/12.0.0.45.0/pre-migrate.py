d22b835c7261cd5660b5338ea0b8a286e746b88e
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-04-14 16:07:26 +0300

    Update modules, make website_animate non-community module

---
+++ yodoo_apps_database/migrations/12.0.0.45.0/pre-migrate.py
@@ -0,0 +1,20 @@
+from odoo.addons.yodoo_apps_database.tools.migration_utils import (
+    sync_module_info_data,
+)
+from odoo.addons.generic_mixin.tools.migration_utils import ensure_version
+
+
+@ensure_version('0.44.0')
+def migrate(cr, installed_version):
+    sync_module_info_data(cr)
+    cr.execute("""
+        UPDATE yodoo_module_serie
+        SET is_odoo_community_addon = False
+        WHERE module_id = (
+            SELECT id
+            FROM yodoo_module
+            WHERE system_name = 'website_animate');
+        UPDATE yodoo_module
+        SET is_odoo_community_module = False
+        WHERE system_name = 'website_animate';
+    """)
