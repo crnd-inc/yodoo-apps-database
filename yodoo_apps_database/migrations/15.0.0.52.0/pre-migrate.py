60df0d10d358796e3dfeda26c0cc81b8b4f1ad34
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-12-04 23:33:23 +0200

    Fix migration for support for Odoo 17

--- yodoo_apps_database/migrations/15.0.0.52.0/pre-migrate.py
+++ yodoo_apps_database/migrations/15.0.0.52.0/pre-migrate.py
@@ -1,9 +1,11 @@
 from odoo.addons.yodoo_apps_database.tools.migration_utils import (
     sync_module_info_data,
+    create_or_update_serie,
 )
 from odoo.addons.generic_mixin.tools.migration_utils import ensure_version
 
 
 @ensure_version('0.52.0')
 def migrate(cr, installed_version):
+    create_or_update_serie(cr, 17, 0)
     sync_module_info_data(cr)
